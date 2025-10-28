5182914866b1ba862ce2b014ec97a9c62d8d6712
Juanca <juancarlosc@onetree.com>
2025-10-27 22:51:04 -0400

    Add FirstStepsApp component and integrate into main render logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,51 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+//<StrictMode>permite asegurarnos si la aplicación funciona como nosotros esperamos</StrictMode>
+//<StrictMode>mantenerlo siempre</StrictMode>
+
+/**
+ * al retirar <StrictMode></StrictMode>, solo permite un elemento html.
+ * con múltiples elementos, se genera un error.
+ */
+// createRoot(document.getElementById('root')!).render(
+//     <h1>Hola Mundo!!</h1>
+// );
+
+
+/**
+ * al retirar <StrictMode></StrictMode>, solo permite un elemento html.
+ * se puede envolver los elementos en un div y funciona sin problemas.
+ */
+// createRoot(document.getElementById('root')!).render(
+//   <div>
+//     <h1>Hola Mundo!!</h1>
+//     <p>Esto es un párrafo</p>
+//     <button>Click me</button>
+//     <div>
+//       <h2>Hola dentro de un div</h2>
+//     </div>
+//   </div>,
+// );
+
+/**
+ * se puedde usar el fragmento <> para envolver los elementos
+ */
+// createRoot(document.getElementById('root')!).render(
+//   <>
+//     <h1>Hola Mundo!!</h1>
+//     <p>Esto es un párrafo</p>
+//     <button>Click me</button>
+//     <div>
+//       <h2>Hola dentro de un div</h2>
+//     </div>
+//   </>,
+// );
+
+import { FirstStepsApp } from "./FirstStepsApp.tsx";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hola Mundo!!</h1>
+    <FirstStepsApp />
   </StrictMode>,
-)
+);
